7f665b04dc40532309f6daf7fc8af5d0f6045d80
AlexandruBaris <a.barisenschi@gmail.com>
2020-12-13 22:31:50 +0200

    created constants

---
+++ src/datatypes/Constants.java
@@ -0,0 +1,35 @@
+package datatypes;
+
+public class Constants {
+    public static void main(String[] args) {
+
+        byte zero = 0;
+        byte one = 1;
+        double pi = Math.PI;
+        double e = Math.E;
+        double two = 2;
+        double square = Math.sqrt(2);
+        String unit = "i^2 = -1.";
+
+
+        System.out.println("* " + zero + "(zero.)\n" +
+                "* " + one + "(one) the natural number after zero.\n" +
+                "* π(pi), the constant representig the ratio of a circle's circumference to its diameter, approximately equal to \n" +
+                " " + pi + ".\n" +
+                "* e, approximately equal to " + e + ".\n" +
+                "* i, the imaginary unit such that " + unit + "\n" +
+                "* √2(square root of 2), the lenght of the diagonal of a square with unit sides, approximately equal to\n" +
+                " " + square + "\n" +
+                "*");
+
+
+    }
+}
+
+
+
+
+
+
+
+
