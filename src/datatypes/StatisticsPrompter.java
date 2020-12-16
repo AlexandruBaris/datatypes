5b6d2bef9b1b304c2b30b9d7076da9e804fe18dd
AlexandruBaris <a.barisenschi@gmail.com>
2020-12-16 17:05:56 +0200

    created statisticsPromter + answers at final + corected

--- src/datatypes/StatisticsPrompter.java
+++ src/datatypes/StatisticsPrompter.java
@@ -6,22 +6,22 @@ public class StatisticsPrompter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Which is the number of the world's population?");
-        long number = scanner.nextLong();
+        long worldPopulation = scanner.nextLong();
 
         System.out.println("Which is the annual growth rate?");
-        double number2 = scanner.nextDouble();
+        double growthRate = scanner.nextDouble();
 
         System.out.println("Which is the first source of information?");
-        String answer = scanner.next();
+        String firstSource = scanner.next();
 
         System.out.println("Which is the second source of information?");
-        String answer2 = scanner.next();
+        String secondSource = scanner.next();
 
         System.out.println("You answered :\n" +
-                "Current World Population is " + number + "\n" +
-                "The annual growth rate is " + number2 + "\n" +
-                "The first source of info is " + answer + "\n" +
-                "The second source is " + answer2);
+                "Current World Population is " + worldPopulation + "\n" +
+                "The annual growth rate is " + growthRate + "\n" +
+                "The first source of info is " + firstSource + "\n" +
+                "The second source is " + secondSource);
 
     }
 }
