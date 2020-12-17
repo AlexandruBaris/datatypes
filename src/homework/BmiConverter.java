296f3b68b375cba13524d241e8274b95ac654bbe
AlexandruBaris <a.barisenschi@gmail.com>
2020-12-17 22:20:57 +0200

    created Body Mass Index (BMI)

---
+++ src/homework/BmiConverter.java
@@ -0,0 +1,24 @@
+package homework;
+
+import java.util.Scanner;
+
+public class BmiConverter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your height in feet and inches: ");
+        double feet = scanner.nextDouble();
+
+        System.out.print("Enter your weight in pounds: ");
+        double pounds = scanner.nextDouble();
+
+        double inches = feet * 12;
+        double meters = inches * 0.0254;
+        double kilograms = pounds / 2.2;
+        double bmi = kilograms / Math.sqrt(meters);
+
+        System.out.print("The Body Mass Index (BMI) is: " + bmi + " kg/m2");
+    }
+}
