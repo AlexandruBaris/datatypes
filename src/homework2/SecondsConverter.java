13c1e1a827d7701dab3f444e7a801573951e923f
AlexandruBaris <a.barisenschi@gmail.com>
2020-12-20 17:31:44 +0200

    created ScondsConverter

--- src/homework2/SecondsConverter.java
+++ src/homework2/SecondsConverter.java
@@ -14,10 +14,10 @@ public class SecondsConverter {
         System.out.print("Enter the number of second: ");
         long input = scanner.nextLong();
 
-        long days = input / (HOURS_IN_DAY * 3600);
-        long hours = (input % (HOURS_IN_DAY * 3600)) / (MINUTES_IN_AN_HOUR * 60);
-        long minutes = ((input % (HOURS_IN_DAY * 3600)) % (MINUTES_IN_AN_HOUR * 60)) / SECONDS_IN_A_MINUTE;
-        long seconds = ((input % (HOURS_IN_DAY * 3600)) % (MINUTES_IN_AN_HOUR * 60)) % SECONDS_IN_A_MINUTE;
+        long days = input / (HOURS_IN_DAY * (MINUTES_IN_AN_HOUR * MINUTES_IN_AN_HOUR));
+        long hours = (input % (HOURS_IN_DAY * (MINUTES_IN_AN_HOUR * MINUTES_IN_AN_HOUR))) / (MINUTES_IN_AN_HOUR * MINUTES_IN_AN_HOUR);
+        long minutes = ((input % (HOURS_IN_DAY * (MINUTES_IN_AN_HOUR * MINUTES_IN_AN_HOUR))) % (MINUTES_IN_AN_HOUR * MINUTES_IN_AN_HOUR)) / SECONDS_IN_A_MINUTE;
+        long seconds = ((input % (HOURS_IN_DAY * (MINUTES_IN_AN_HOUR * MINUTES_IN_AN_HOUR))) % (MINUTES_IN_AN_HOUR * SECONDS_IN_A_MINUTE)) % SECONDS_IN_A_MINUTE;
 
         System.out.println(input + " seconds = " + days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds");
     }
