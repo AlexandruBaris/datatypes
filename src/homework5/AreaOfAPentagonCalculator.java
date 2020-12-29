83aa53fe7dcc242698cfaff6551b9b6724d148b2
AlexandruBaris <a.barisenschi@gmail.com>
2020-12-29 15:27:15 +0200

    created AreaCalculator

--- src/homework5/AreaOfAPentagonCalculator.java
+++ src/homework5/AreaOfAPentagonCalculator.java
@@ -7,9 +7,9 @@ public class AreaOfAPentagonCalculator {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the length from the center to a vertex: ");
-        double length_R = scanner.nextDouble();
-        double length_of_a_side = 2 * length_R * Math.sin(Math.PI / 5);
-        double area = (5 * Math.pow(length_of_a_side, 2) / (4 * Math.tan(Math.PI / 5)));
+        double radius = scanner.nextDouble();
+        double lengthOfASide = 2 * radius * Math.sin(Math.PI / 5);
+        double area = (5 * Math.pow(lengthOfASide, 2) / (4 * Math.tan(Math.PI / 5)));
         System.out.printf("The are of the pentagon is %.2f", area);
 
     }
