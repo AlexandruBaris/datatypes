56201d35e7a630cd478176003e8437b2269e5dc0
AlexandruBaris <a.barisenschi@gmail.com>
2021-01-04 12:29:02 +0200

    created FatGramCalculator

---
+++ src/homework6/FatGramCalculator.java
@@ -0,0 +1,20 @@
+package homework6;
+
+import java.util.Scanner;
+
+public class FatGramCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter total number of calories: ");
+        double totCalories = input.nextDouble();
+        System.out.print("Enter the amount of fat in grams: ");
+        double totFat = input.nextDouble();
+        double caloriesFromFat = totFat * 9;
+        double percentageOfCalories = caloriesFromFat % totCalories;
+        if (percentageOfCalories < 30) {
+            System.out.println("The food is low in fat.");
+        } else {
+            System.out.println("The percentage of the calories that come from fat is " + percentageOfCalories);
+        }
+    }
+}
