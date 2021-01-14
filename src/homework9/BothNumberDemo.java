986cab22173a24334eda1d22c593df82241efbf3
AlexandruBaris <a.barisenschi@gmail.com>
2021-01-14 18:05:14 +0200

    created divisible numbers

--- src/homework9/BothNumberDemo.java
+++ src/homework9/BothNumberDemo.java
@@ -1,17 +1,7 @@
 package homework9;
 
 public class BothNumberDemo {
-    public static void divider(int number, int line) {
-        while (number <= 1000) {
-            if (number % 5 == 0 && number % 6 == 0) {
-                System.out.print(number + " ");
-                line++;
-                if (line % 10 == 0) {
-                    System.out.println();
-                    line++;
-                }
-            }
-            number++;
-        }
+    public static void main(String... args) {
+        BothNumbersDivider.divider(100, 1);
     }
 }
--- src/homework9/DemoNumbers.java
+++ src/homework9/DemoNumbers.java
@@ -1,17 +1,7 @@
 package homework9;
 
 public class DemoNumbers {
-    public static void calculator(int number, int line) {
-        while (number <= 1000) {
-            if (number % 5 == 0 || number % 6 == 0) {
-                System.out.print(number + " ");
-                line++;
-            }
-            if (line % 10 == 0) {
-                System.out.println();
-                line++;
-            }
-            number++;
-        }
+    public static void main(String... args) {
+        DivisibleNumbers.calculator(100, 1);
     }
 }
