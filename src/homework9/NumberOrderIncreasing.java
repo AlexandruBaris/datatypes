69cec833d178df12c63cf45739fc8d4cddc5ae96
AlexandruBaris <a.barisenschi@gmail.com>
2021-01-14 23:44:49 +0200

    created divisible FeetAndMetersConvertor

--- src/homework9/NumberOrderIncreasing.java
+++ src/homework9/NumberOrderIncreasing.java
@@ -3,21 +3,16 @@ package homework9;
 import java.util.Scanner;
 
 public class NumberOrderIncreasing {
+
     public static void main(String... args) {
 
-    }
-
-    public static void displayPattern(int num1, int num2, int num3) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter 3 numbers(separated by space): ");
-        num1 = scanner.nextInt();
-        num2 = scanner.nextInt();
-        num3 = scanner.nextInt();
-
-        if (num1 <= num2) {
-            System.out.println();
+        //num1 = scanner.nextInt();
+        //num2 = scanner.nextInt();
+        //num3 = scanner.nextInt();
 
 
-        }
     }
-}
+    }
+
---
+++ src/homework9/Test.java
@@ -0,0 +1,8 @@
+package homework9;
+
+public class Test {
+    public static void main(String... args) {
+        double x = 32;
+        System.out.printf("", x);
+    }
+}
