3d7c46509472a3754821733bfa77ce511c20a2bf
AlexandruBaris <a.barisenschi@gmail.com>
2021-01-17 14:15:00 +0200

    created PatternsDisplay

--- src/homework9/NumberOrderIncreasing.java
+++ src/homework9/NumberOrderIncreasing.java
@@ -1,12 +1,15 @@
 package homework9;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class NumberOrderIncreasing {
 
     public static void main(String... args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String[] array = {"Alex", "Vas", "Ion", "Adrian", "Mar"};
+
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
 
 
     }
