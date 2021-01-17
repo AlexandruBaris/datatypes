1c1b036d70178ca97544be08394271fb7a1a3395
AlexandruBaris <a.barisenschi@gmail.com>
2021-01-17 14:44:34 +0200

    created DisplayCharacters

---
+++ src/homework10/DisplayCharactersDemo.java
@@ -0,0 +1,9 @@
+package homework10;
+
+public class DisplayCharactersDemo {
+    public static final int CHARS_PER_LINE = 10;
+
+    public static void main(String... args) {
+        DisplayCharacters.printChars((char) 1, 'Z', CHARS_PER_LINE);
+    }
+}
