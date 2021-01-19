3e8d78322b32c2da474e50cb9e1b7e985d889bd4
AlexandruBaris <a.barisenschi@gmail.com>
2021-01-19 15:35:59 +0200

    created Summation

---
+++ src/homework11/Summation.java
@@ -0,0 +1,11 @@
+package homework11;
+
+public class Summation {
+    public static void main(String... args) {
+        double summation = 0.0;
+        for (double i = 1.0; i <= 625.0; i++) {
+            summation += 1 / (Math.sqrt(i) + Math.sqrt(i + 1));
+        }
+        System.out.println("Summation: " + summation);
+    }
+}
