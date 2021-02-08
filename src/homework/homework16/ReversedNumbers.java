89ae7d7e7e0711de4b1b032380b0319e536927c1
AlexandruBaris <a.barisenschi@gmail.com>
2021-02-08 13:42:20 +0200

    created ReversedNumbers

--- src/homework/homework16/ReversedNumbers.java
+++ src/homework/homework16/ReversedNumbers.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class ReversedNumbers {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
-        int[] num = new int[10];
         System.out.print("Enter 10 integers separated by a space: ");
         String[] numbers = scanner.nextLine().split(" ");
 
