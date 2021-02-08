005d6e19aac5931af8f564ab9993c6a5bae5bb59
AlexandruBaris <a.barisenschi@gmail.com>
2021-02-08 13:40:29 +0200

    created ReversedNumbers

--- src/homework/homework16/ReversedNumbers.java
+++ src/homework/homework16/ReversedNumbers.java
@@ -1,12 +1,24 @@
 package homework.homework16;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ReversedNumbers {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
+        int[] num = new int[10];
+        System.out.print("Enter 10 integers separated by a space: ");
+        String[] numbers = scanner.nextLine().split(" ");
 
-        System.out.print("Enter 10 integers: ");
-
+        reverse(numbers);
     }
+
+    private static void reverse(String[] numbers) {
+        Collections.reverse(Arrays.asList(numbers));
+        System.out.print(Arrays.asList(numbers));
+    }
+
+
 }
+
