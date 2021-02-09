2e7faa801a0796341201d502ca09bee6f5675279
AlexandruBaris <a.barisenschi@gmail.com>
2021-02-09 10:51:15 +0200

    created SumOfNumbers

--- src/homework/homework17/SumOfNumbers.java
+++ src/homework/homework17/SumOfNumbers.java
@@ -1,23 +1,17 @@
 package homework.homework17;
 
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class SumOfNumbers {
     public static void main(String... args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a series of numbers separated by commas: ");
         String[] numbers = scanner.next().split(",");
-        Integer[] ints = Stream.of(numbers).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
-        int sum = 0;
-        for (Integer anInt : ints) {
-            sum += anInt;
-        }
-        System.out.println("Sum = " + sum);
-        inter(numbers);
+
+        sumOfDigits(numbers);
     }
 
-    public static void inter(String[] numbers) {
+    public static void sumOfDigits(String[] numbers) {
         Integer[] array = new Integer[numbers.length];
         for (int i = 0; i < array.length; i++) {
             array[i] = Integer.parseInt(numbers[i]);
